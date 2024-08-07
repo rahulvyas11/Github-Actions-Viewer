42772705e0d155e55d97c83b518d8b5c242d3a28
rahulvyas11 <rvyas@wisc.edu>
2024-08-07 18:52:01 -0500

    Finished Application

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23li63DvWqLp6tpXpg';
+const githubClientSecret = 'b18fd64758608e2ba738b7aeb63ac80ce13171c0';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
